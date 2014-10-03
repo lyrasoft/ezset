39c1b8801da72c3f4b5b8db670a1fd971dbf7e20
Simon Asika <asika32764@gmail.com>
2014-10-03 11:01:27 +0800

    Add example listener and init hook

---
+++ ezset/resources/ezset/src/init.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Part of Ezset project.
+ *
+ * @copyright  Copyright (C) 2014 ASIKART.com. All rights reserved.
+ * @license    GNU General Public License version 2 or later;
+ */
+
+// Your logic here
